dfdeb07c8a105bd082a6d7e024f02cb2a0f34549
Sashank Rampalli <66544316+Sashankr@users.noreply.github.com>
2023-05-03 07:10:24 +0530

    Added Custom layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../Sidebar";
+import Navbar from "../Navbar";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Sidebar />
+      <Navbar />
       {children}
     </div>
   );
