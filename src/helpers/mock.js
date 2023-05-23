fb2bad33c60f206cb171fdf43c4e24b2a70a79d7
Sashank Rampalli <66544316+Sashankr@users.noreply.github.com>
2023-05-23 08:46:37 +0530

    updated assets

---
+++ src/helpers/mock.js
@@ -0,0 +1,32 @@
+export const homePageSongsList = [
+  {
+    name: "Storm Clouds",
+    source: "/public/songs/storm-clouds-purpple-cat.mp3",
+    imageSource: "/public/song-1.jpg",
+    artist: "Purple Cat",
+  },
+  {
+    name: "Still Awake",
+    source: "/public/songs/Still-Awake-Lofi-Study-Music.mp3",
+    imageSource: "/public/song-2.jpg",
+    artist: "Ghostrifter Official",
+  },
+  {
+    name: "Morning Routine",
+    source: "/public/songs/Morning-Routine-Lofi-Study-Music.mp3",
+    imageSource: "/public/song-3.jpg",
+    artist: "Ghostrifter Official",
+  },
+  {
+    name: "Heart Of The Ocean",
+    source: "/public/songs/Heart-Of-The-Ocean.mp3",
+    imageSource: "/public/song-4.jpg",
+    artist: "Purple Cat",
+  },
+  {
+    name: "City Lights",
+    source: "/public/songs/Ghostrifter-Official-City-Lights.mp3",
+    imageSource: "/public/song-5.jpg",
+    artist: "Ghostrifter Official",
+  },
+];
