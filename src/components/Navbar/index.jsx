ac3e4cd9e415dd2402674d6b93ffa966c642ae1d
Sashank Rampalli <66544316+Sashankr@users.noreply.github.com>
2023-05-23 07:41:15 +0530

    updated body background

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
-  return <div className={styles.navbar}>Navbar</div>;
+  return (
+    <div className={styles.navbar}>
+      <nav>
+        <section className={styles["navbar__navigate"]}>
+          <i class="fa-solid fa-chevron-left"></i>
+          <i class="fa-solid fa-chevron-right"></i>
+        </section>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
